89f98b72a1b2f948f04233e21d182561e95c5308
Kateryna Zabolotna <katerynazabolotna19@gmail.com>
2018-12-05 21:24:27 +0200

    fix

---
+++ src/com/company/Arithmetic/demo.java
@@ -0,0 +1,4 @@
+package com.company.Arithmetic;
+
+public class demo {
+}
---
+++ src/com/company/CatDog.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class CatDog {
+  public int height = 180;
+  public String name = "Default";
+  public void say(String name){
+      System.out.println("Hello, " + name );
+  }
+  public CatDog() {};
+  public CatDog (int h, String n) {
+      height = h;
+      name = n;
+    }
+    public CatDog (String n) {
+      name = n;
+    }
+}
