1a8000502b6e028319984a545ef4bde6b07fa111
Kateryna Zabolotna <katerynazabolotna19@gmail.com>
2019-01-21 21:41:49 +0200

    delete

--- src/project/Project.java
+++ src/project/Project.java
@@ -1,7 +1,5 @@
 package project;
 
-import java.util.Random;
-
 public class Project {
 
     public static void main(String[] args) {
@@ -9,9 +7,6 @@ public class Project {
         System.out.print(args[0] + " ");
         System.out.print(args[1] + " ");
         System.out.println(args[2]);
-
-        Random r = new Random();
-        System.out.println("Random number: " + r.nextInt(1000));
     }
 }
 
