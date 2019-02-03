ea9122f3d56b14e9482324918226e539fd7d47a1
Kateryna Zabolotna <katerynazabolotna19@gmail.com>
2019-02-03 13:53:24 +0200

    fix

--- src/project/Project.java
+++ src/project/Project.java
@@ -3,11 +3,16 @@ package project;
 public class Project {
 
     public static void main(String[] args) {
+        String familyName = "Kozak";
+        String firstName = "Iurii";
+        String fatherName = "Andrijovych";
 
-        System.out.print(args[0] + " ");
-        System.out.print(args[1] + " ");
-        System.out.println(args[2]);
+        if (args[0].equals(familyName)
+                && args[1].equals(firstName)
+                && args[2].equals(fatherName)) {
+            System.out.print(args[0] + " ");
+            System.out.print(args[1] + " ");
+            System.out.println(args[2]);
+        } else System.out.println("Wrong name!");
     }
 }
-
-
